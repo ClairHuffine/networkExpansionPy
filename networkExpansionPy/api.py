c3bd52a9a600aa8ae01de403f95fbd021a3c05c2
ClairHuffine <clair.huffine@gmail.com>
2023-10-02 13:36:24 -0600

    pH and Thermo
    Added pH and thermo params

--- networkExpansionPy/api.py
+++ networkExpansionPy/api.py
@@ -12,22 +12,27 @@ def read_root():
 
 
 @app.get("/expand")
-def expand_network(seedSet: list[str] = Query(None)):
+def expand_network(seedSet: list[str] = Query(None), pH=7.0):
     """
     Expand the network given a seed set of compounds.
 
     Takes a list of compound IDs and returns a dictionary with two keys:
     - rxns: a list of reaction IDs
     - compounds: a list of compound IDs
-
+    
+    Optional Variables
+    -pH: float from 5.0 to 9.0 in 0.5 increments
+    
     Example:
     """
     network = GlobalMetabolicNetwork()
     network.convertToIrreversible()
     network.pruneInconsistentReactions()
-    # network.pruneThermodynamicallyInfeasibleReactions()
+    network.setMetaboliteBounds()
+    network.set_ph(pH)
+    network.pruneThermodynamicallyInfeasibleReactions()
     compounds, rxns = network.expand(seedSet)
-    return {"seeds": seedSet, "rxns": rxns, "compounds": compounds}
+    return {"pH":pH, "seeds": seedSet, "rxns": rxns, "compounds": compounds}
 
 
 if __name__ == "__main__":
